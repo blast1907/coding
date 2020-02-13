c1563bac29bb9e2d9f1588c3cd840218d670011c
H S Guna Sheakar <43476636+blast1907@users.noreply.github.com>
2020-02-13 23:31:39 +0530

    Create No_of_bst(dp).cpp

---
+++ No_of_bst(dp).cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+void no_of_bst(int n)
+{
+    int a[n+1];
+    a[0]=1;
+    a[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        int count1=0;
+        for(int j=0;j<i;j++)
+            count1+=(a[j]*a[i-j-1]);
+        a[i]=count1;
+    }
+    return a[n];   
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<no_of_bst(n)<<endl;
+}
